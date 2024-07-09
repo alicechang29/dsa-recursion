924b93f128ce94448953aa43f87a8e88f7690da2
alicechang29 <147457604+alicechang29@users.noreply.github.com>
2024-07-09 16:10:21 -0700

    findIndex

--- recursion/find-index/findIndex.ts
+++ recursion/find-index/findIndex.ts
@@ -1,7 +1,31 @@
+/*
+empty array
+
+non empty
+[1,2,3] - 3
+
+1(f([1,2,3])) - 1
+  2([2,3]) -  1
+    3f([3]) - 0
+
+*/
+
 /** findIndex: return index of val in arr (or -1 if val is not present). */
 
 function findIndex(arr: number[], val: number): number {
-  return 42;
+  const currNum = arr[0]; //1
+  //base case
+  if (currNum === val) return 0; // 0
+  if (arr.length === 0) return -1;
+
+  const indexCount = findIndex(arr.slice(1), val); // [2,3], 3
+
+  //going down
+  if (indexCount !== -1) {
+    return indexCount + 1;
+  }
+
+  return -1;
 }
 
 export { findIndex };
