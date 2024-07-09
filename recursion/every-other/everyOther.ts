f56485bbb55dc576ea2d6ce827e2db439f3d27c0
Anya Aven <leafeon96@gmail.com>
2024-07-09 15:43:47 -0400

    Finish everyOther

--- recursion/every-other/everyOther.ts
+++ recursion/every-other/everyOther.ts
@@ -1,7 +1,21 @@
+
+/** Pseudo-Code
+ * get the first char
+ * recursively call with the string - 2
+ *
+ * "hello"
+ * 1(hello) h + 2()
+ *    2(llo) l + 3()
+ *      3(o) o + 4()
+ *          4("") (base case, string.length === 0) -> ""
+ */
+
 /** everyOther: return a string with every other letter. */
 
 function everyOther(str: string): string {
-  return "x";
+  if(str.length === 0) return ""; // Base case
+
+  return str[0] + everyOther(str.slice(2));
 }
 
 export { everyOther };
