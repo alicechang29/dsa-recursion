862c04758ccc2425d7449b11b258fea422c75a17
alicechang29 <147457604+alicechang29@users.noreply.github.com>
2024-07-09 15:33:15 -0700

    palindrome

--- recursion/is-palindrome/isPalindrome.ts
+++ recursion/is-palindrome/isPalindrome.ts
@@ -1,7 +1,24 @@
+/*
+
+if ends are the same, continue
+
+*/
+
 /** isPalindrome: checks whether a string is a palindrome or not. */
 
 function isPalindrome(str: string): boolean {
-  return true;
+  //base case - midpoint
+  console.log(str);
+  if (str.length < 2) return true;
+
+  const firstLetter = str[0];
+  const lastLetter = str[str.length - 1];
+
+  if (firstLetter === lastLetter) {
+    return isPalindrome(str.slice(1, str.length - 1));
+  }
+
+  return false;
 }
 
 export { isPalindrome };
