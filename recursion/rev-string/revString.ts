8d22601f633544f2c6a6e6eba6a541335c165b38
Anya Aven <leafeon96@gmail.com>
2024-07-09 18:41:02 -0400

    Finish revString

--- recursion/rev-string/revString.ts
+++ recursion/rev-string/revString.ts
@@ -1,7 +1,18 @@
+
+/*
+1("str") 2() + s
+    2("tr") 3() + t
+      3("r") 4() + r
+        4("")
+results: "rts"
+*/
+
 /** revString: return a copy of a string, but in reverse. */
 
 function revString(str: string): string {
-  return "x";
+  if(str.length === 0) return str
+  const firstChar = str[0];
+  return revString(str.slice(1)) + firstChar;
 }
 
 export { revString };
