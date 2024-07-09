3a922a3e92ad6ac1f479c6cf9a7389694b4004eb
alicechang29 <147457604+alicechang29@users.noreply.github.com>
2024-07-09 12:32:28 -0700

    Add Product

--- recursion/product/product.ts
+++ recursion/product/product.ts
@@ -1,9 +1,34 @@
 import { LLNodeNum } from "../common/ll";
 
+/*
+empty list
+
+non empty list
+- to start: get val of head
+- set curr node to the head.next
+- get the val of curr node.val
+- set curr node to curr node.next
+- do this until we hit length of 0
+
+
+[2,3,4]
+2
+3
+4
+0
+*/
+
 /** product: calculate the product of a linked list of numbers. */
 
 function product(nums: LLNodeNum | null): number {
-  return 42;
+  //base case
+  if (nums === null) {
+    return 1;
+  }
+  // go until we get null
+  let currNodeVal = nums.val;
+  let nextNode = nums.next;
+  return currNodeVal * product(nextNode);
 }
 
 export { product };
