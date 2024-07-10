fa279ad11c386e982fee04f3507a186213773db9
alicechang29 <147457604+alicechang29@users.noreply.github.com>
2024-07-09 18:17:40 -0700

    binarysearch index

--- recursion/binary-search-index/binarySearchIndex.ts
+++ recursion/binary-search-index/binarySearchIndex.ts
@@ -1,12 +1,30 @@
+/*
+
+
+
+*/
+
 /** binarySearch: given a sorted array of numbers, and a value,
  * return the index of that value (or -1 if val is not present). */
 
 function binarySearchIndex(
-    arr: number[],
-    val: number,
-    left = 0,
-    right = arr.length): number {
-  return 42;
+  arr: number[],
+  val: number,
+  left = 0,
+  right = arr.length): number {
+
+  const midpoint = Math.floor(((left + right) / 2));
+  const midVal = arr[midpoint];
+
+  if (midVal === val) return midpoint;
+  if (left >= right - 1) return -1;
+
+  if (val > midVal) { // Right half search
+    return binarySearchIndex(arr, val, midpoint, right);
+  }
+  else { // Left half search
+    return binarySearchIndex(arr, val, left, midpoint);
+  }
 }
 
 export { binarySearchIndex };
